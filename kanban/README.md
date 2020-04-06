5f4201f3ee5e6b06f3a56fcf06f2a0100012b755
pavan <dongare.pavan25@gmail.com>
2020-04-07 01:31:41 +0530

    firebase

--- kanban/README.md
+++ kanban/README.md
@@ -28,3 +28,5 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 
 ----
 ng add @angular/material
+install firebase
+
--- kanban/src/app/user/google-signin.directive.ts
+++ kanban/src/app/user/google-signin.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from '@firebase/app';// import core firebase client (required)
+import '@firebase/auth'; // import Firebase Authentication (optional)
+import '@firebase/database'; // import Firebase Realtime Database (optional)
+import '@firebase/firestore'; // import Cloud Firestore (optional)
 
 @Directive({
   selector: '[appGoogleSignin]'
 })
 export class GoogleSigninDirective {
 
-  constructor() { }
+  constructor(private authService: AngularFireAuth) { }
+
+  @HostListener('click')
+  onclick(){
+    this.authService.signInWithPopup(
+      new firebase.auth.GoogleAuthProvider()
+    );
+     // this.authService.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
 
 }
