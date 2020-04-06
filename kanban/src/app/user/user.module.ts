e46922053d6afcd8f4f79d60b998c0a012116710
pavan <dongare.pavan25@gmail.com>
2020-04-06 21:19:38 +0530

    added new directive

--- kanban/src/app/user/user.module.ts
+++ kanban/src/app/user/user.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './login/login.component';
+import { GoogleSigninDirective } from './google-signin.directive';
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, GoogleSigninDirective],
   imports: [
     CommonModule,
     UserRoutingModule,
