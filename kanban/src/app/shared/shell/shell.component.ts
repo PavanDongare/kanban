65290c0cefd697fb4e521741b7999e2d673211af
pavan <dongare.pavan25@gmail.com>
2020-04-06 15:13:23 +0530

    navbar

--- kanban/src/app/shared/shell/shell.component.ts
+++ kanban/src/app/shared/shell/shell.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { BreakpointObserver, Breakpoints  } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map , shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shell',
@@ -8,7 +11,14 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class ShellComponent implements OnInit {
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
 
   ngOnInit(): void {
   }
