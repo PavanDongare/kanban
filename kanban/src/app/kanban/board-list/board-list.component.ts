508f18af09bd1e6925d768860c76f481d8177de0
pavan <dongare.pavan25@gmail.com>
2020-04-12 03:01:06 +0530

    subscription to boardservice

--- kanban/src/app/kanban/board-list/board-list.component.ts
+++ kanban/src/app/kanban/board-list/board-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { BoardService } from './../board.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Board } from '../board.model';
 
 @Component({
   selector: 'app-board-list',
   templateUrl: './board-list.component.html',
   styleUrls: ['./board-list.component.scss']
 })
-export class BoardListComponent implements OnInit {
+export class BoardListComponent implements OnInit , OnDestroy{
 
-  constructor() { }
+  constructor(private boardService : BoardService) { }
+  boards: Board [];
+  sub: Subscription;
+
+
 
   ngOnInit(): void {
+    this.sub = this.boardService.getUserBoards().subscribe(boards=> (this.boards = boards));
+  }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
 }
